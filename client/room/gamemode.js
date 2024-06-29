90b7f9952135f49104f0d67d83ca6e58c5db2e4d
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-06-30 00:08:19 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -1,6 +1,7 @@
 //var System = importNamespace('System');
 import { DisplayValueHeader, Color } from 'pixel_combats/basic';
 import { Game, Players, Inventory, LeaderBoard, BuildBlocksSet, Teams, Damage, BreackGraph, Ui, Properties, GameMode, Spawns, Timers, TeamsBalancer } from 'pixel_combats/room';
+import * as default_timer from './default_timer.js';
 
 // Константы
 var WaitingPlayersTime = 5;
