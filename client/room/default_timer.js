ae031b521df1a26c89dd92fe627ff7a5d9e0c763
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-06-30 00:04:26 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -1,1 +1,14 @@
+import { GameMode } from 'pixel_combats/room';
+
+// Константы, времении
+var PARAMETER_GAME_LENGTH = 'default_game_mode_length';
+
+// Длинна матча, каждого раунда
+export function game_mode_length_seconds() {
+var length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
+switch (length) {
+  case 'length_Короткая(2 минуты)': return 240; // 4 min 
+  case 'length_Средняя(4 минут)': return 420; // 5 min 
+  case 'length_Длинная(7 минут)': return 
+
 
