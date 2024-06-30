8992a08343f2533c5ee34cf4c57babbdda0996f5
SSoDm <163316535+SSoDm@users.noreply.github.com>
2024-06-30 08:56:31 +0700

    Update default_timer.js

--- client/room/default_timer.js
+++ client/room/default_timer.js
@@ -7,10 +7,10 @@ var PARAMETER_GAME_LENGTH = 'default_game_mode_length';
 export function game_mode_length_seconds() {
 var length = GameMode.Parameters.GetString(PARAMETER_GAME_LENGTH);
 switch (length) {
-  case 'length_КОРОТКАЯ': return 240; // 4 min 
-  case 'length_СРЕДНЯЯ': return 300; // 5 min
-  case 'length_ДЛИННАЯ': return 360; // 6 min
-  case 'length_ДЛИТЕЛЬНАЯ': return 420; // 7 min
+  case 'length_КОРОТКАЯ': return 230; // 4 min 
+  case 'length_СРЕДНЯЯ': return 520; // 5 min
+  case 'length_ДЛИННАЯ': return 720; // 6 min
+  case 'length_ДЛИТЕЛЬНАЯ': return 850; // 7 min
 }
 return 300;
 }
