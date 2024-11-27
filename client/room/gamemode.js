eb0a6306309baa14516c958ec0c7fa4c7864d511
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-11-27 15:49:39 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -154,8 +154,8 @@ SetWaitingMode();
 function SetWaitingMode() {
 	stateProp.Value = WaitingStateValue;
 	Ui.GetContext().Hint.Value = "Ожидание, игроков...";
-	Spawns.GetContext().Enable = true;
-	Spawns.Despawn();
+	Spawns.GetContext().Enable = false;
+	Spawns.GetContext().Despawn();
 	mainTimer.Restart(WaitingPlayersTime);
 }
 
@@ -171,6 +171,7 @@ function SetBuildMode()
 	Inventory.Build.Value = true;
 
 	mainTimer.Restart(BuildBaseTime);
+        Spawns.GetContext().Enable = true;
         Spawns.GetContext().Spawn();
 	SpawnTeams();
 }
