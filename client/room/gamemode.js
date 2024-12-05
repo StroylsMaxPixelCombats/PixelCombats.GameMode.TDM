04f8d7b1424af18373475cf3bfc1cf3d0d8df9e2
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2024-12-05 18:09:43 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -25,6 +25,9 @@ Damage.FriendlyFire = GameMode.Parameters.GetBool("FriendlyFire");
 Map.Rotation = GameMode.Parameters.GetBool("MapRotation");
 BreackGraph.OnlyPlayerBlocksDmg = GameMode.Parameters.GetBool("PartialDesruction");
 BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
+ if (GameMode.Parameters.GetBool("Blocks29")) {
+Player.ContextedProperties.StartBlocksCount.Value = true;
+}
 
 // Блок игрока, всегда - усилен:
 BreackGraph.PlayerBlockBoost = true;
